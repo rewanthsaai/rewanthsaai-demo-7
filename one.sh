60538809535ca0dd23d6416ba5951cdf9bd11d6c
rewanthsaai <rewanthsaai@gmail.com>
2022-08-01 14:16:09 +0530

    linux_commands

---
+++ one.sh
@@ -0,0 +1,17 @@
+
+
+
+git commands 
+
+
+            git init 
+
+	    git clone <url> 
+
+	    git remote add origin <url> 
+
+	    touch .txt <to create a file> 
+	     
+	    vi <edit a file> 
+
+
