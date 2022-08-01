6b8db914ee5645afb631b366e3aeefd7e57e4639
rewanthsaai <rewanthsaai@gmail.com>
2022-08-01 14:26:37 +0530

    commands

---
+++ two.sh
@@ -0,0 +1,22 @@
+
+
+
+git commands 
+
+
+            git add file name 
+
+	    git commit -m "massage" 
+
+	    git push origin master 
+
+
+	    git branch <create a branch > 
+
+	    git checkout < switch to branch>
+
+	    vi edit the file 
+
+
+
+
